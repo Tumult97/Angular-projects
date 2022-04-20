06ed9976f7442bf2e198f9f33185ac94aafa4b5c
Tristan van der Merwe <tristanvdm87@gmail.com>
2022-04-20 16:40:03 +0200

    Added submit logic to form

--- Forms/src/app/app.component.ts
+++ Forms/src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     //can use two way data binding to do this as well
@@ -40,6 +48,11 @@ export class AppComponent {
   // }
 
   onSubmit(){
-    console.log(this.signupForm.form);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.questionData.secret;
+    this.user.answer = this.signupForm.value.questionData.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    this.submitted = true;
   }
 }
