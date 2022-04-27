965a01f942e0ca31ab6b407b99cd0dffb6c8d2b0
Tristan van der Merwe <tristanvdm87@gmail.com>
2022-04-27 13:21:04 +0200

    Added post and fetch from firebase realtime DB

--- HTTP/src/app/app.component.ts
+++ HTTP/src/app/app.component.ts
@@ -1,28 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts = [];
+  httpPostsPostSub: Subscription;
+  httpPostsGetSub: Subscription;
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
+    this.httpPostsPostSub = this.http.post(
+      'https://ng-test-build-default-rtdb.europe-west1.firebasedatabase.app/posts.json', 
+      postData
+    ).subscribe(resp => {
+      console.log(resp);
+    });
     console.log(postData);
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts(){
+    this.httpPostsGetSub = this.http.get('https://ng-test-build-default-rtdb.europe-west1.firebasedatabase.app/posts.json').subscribe(resp => {
+      console.log(resp);
+    });
+  }
+
+  ngOnDestroy(): void {
+    
   }
 }
