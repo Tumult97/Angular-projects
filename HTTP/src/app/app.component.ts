0186e38317519547fe8ce17549cd05da67d200bc
Tristan van der Merwe <tristanvdm87@gmail.com>
2022-04-27 13:57:39 +0200

    Added data loading and pipes

--- HTTP/src/app/app.component.ts
+++ HTTP/src/app/app.component.ts
@@ -1,50 +1,65 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Post } from './post.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, OnDestroy {
-  loadedPosts = [];
-  httpPostsPostSub: Subscription;
-  httpPostsGetSub: Subscription;
+	loadedPosts: Post[] = [];
+	httpPostsPostSub: Subscription;
+	httpPostsGetSub: Subscription;
+	isLoading: boolean = false;
 
-  constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient) { }
 
-  ngOnInit() {
-    this.fetchPosts();
-  }
+	ngOnInit() {
+		this.fetchPosts();
+	}
 
-  onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
-    this.httpPostsPostSub = this.http.post(
-      'https://ng-test-build-default-rtdb.europe-west1.firebasedatabase.app/posts.json', 
-      postData
-    ).subscribe(resp => {
-      console.log(resp);
-    });
-    console.log(postData);
-  }
+	onCreatePost(postData: Post) {
+		// Send Http request
+		this.httpPostsPostSub = this.http.post<{name: string, }>(
+			'https://ng-test-build-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+			postData
+		).subscribe(resp => {
+			//this.fetchPosts();
+		});
+	}
 
-  onFetchPosts() {
-    // Send Http request
-    this.fetchPosts();
-  }
+	onFetchPosts() {
+		// Send Http request
+		this.fetchPosts();
+	}
 
-  onClearPosts() {
-    // Send Http request
-  }
+	onClearPosts() {
+		// Send Http request
+	}
 
-  private fetchPosts(){
-    this.httpPostsGetSub = this.http.get('https://ng-test-build-default-rtdb.europe-west1.firebasedatabase.app/posts.json').subscribe(resp => {
-      console.log(resp);
-    });
-  }
+	private fetchPosts() {
+		this.isLoading = true;
+		this.httpPostsGetSub = this.http
+			.get<{[key: string]: Post}>('https://ng-test-build-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+			.pipe(map((resp) => {
+				const posts: Post[] = [];
+				for(const key in resp){
+					if(resp.hasOwnProperty(key))
+						posts.push({...resp[key], id: key});
+				}
+				return posts;
+			}))
+			.subscribe(resp => {
+				this.isLoading = false;
+				this.loadedPosts = resp;
+			});
+	}
 
-  ngOnDestroy(): void {
-    
-  }
+	ngOnDestroy(): void {
+		this.httpPostsGetSub.unsubscribe();
+		this.httpPostsPostSub.unsubscribe();
+	}
 }
